8e93afd8fb117ad7b9f2c97285424b389ff707db
UniverseHubScripts <143378033+UniverseHubScripts@users.noreply.github.com>
2023-11-08 01:35:06 -0300

    Update install.ps1

--- resources/install.ps1
+++ resources/install.ps1
@@ -1,11 +1,11 @@
 $ErrorActionPreference = 'Stop'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-Write-Host -Object 'Setting up...' -ForegroundColor 'Cyan'
+Write-Host -Object 'Inciando Download...' -ForegroundColor 'Cyan'
 
 if (-not (Get-Command -Name 'spicetify' -ErrorAction 'SilentlyContinue')) {
-  Write-Host -Object 'Spicetify not found.' -ForegroundColor 'Yellow'
-  Write-Host -Object 'Installing it for you...' -ForegroundColor 'Cyan'
+  Write-Host -Object 'Arquivo Nao Localizado.' -ForegroundColor 'Red'
+  Write-Host -Object 'Instalando Para Você' -ForegroundColor 'Green'
   $Parameters = @{
     Uri             = 'https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1'
     UseBasicParsing = $true
@@ -27,11 +27,11 @@ $isThemeInstalled = $(
 $currentTheme = (spicetify config current_theme)
 $setTheme = $true
 
-Write-Host -Object 'Removing and creating Marketplace folders...' -ForegroundColor 'Cyan'
+Write-Host -Object 'Removendo Arquivos e Criando Pastas Para o Marketplace...' -ForegroundColor 'Cyan'
 Remove-Item -Path $marketAppPath, $marketThemePath -Recurse -Force -ErrorAction 'SilentlyContinue' | Out-Null
 New-Item -Path $marketAppPath, $marketThemePath -ItemType 'Directory' -Force | Out-Null
 
-Write-Host -Object 'Downloading Marketplace...' -ForegroundColor 'Cyan'
+Write-Host -Object 'Baixando Marketplace...' -ForegroundColor 'Cyan'
 $marketArchivePath = "$marketAppPath\marketplace.zip"
 $unpackedFolderPath = "$marketAppPath\spicetify-marketplace-dist"
 $Parameters = @{
@@ -41,7 +41,7 @@ $Parameters = @{
 }
 Invoke-WebRequest @Parameters
 
-Write-Host -Object 'Unzipping and installing...' -ForegroundColor 'Cyan'
+Write-Host -Object 'Extraindo e Instalando...' -ForegroundColor 'Cyan'
 Expand-Archive -Path $marketArchivePath -DestinationPath $marketAppPath -Force
 Move-Item -Path "$unpackedFolderPath\*" -Destination $marketAppPath -Force
 Remove-Item -Path $marketArchivePath, $unpackedFolderPath -Force
@@ -49,7 +49,7 @@ spicetify config custom_apps spicetify-marketplace- -q
 spicetify config custom_apps marketplace
 spicetify config inject_css 1 replace_colors 1
 
-Write-Host -Object 'Downloading placeholder theme...' -ForegroundColor 'Cyan'
+Write-Host -Object 'Baixando Espaco para Temas...' -ForegroundColor 'Cyan'
 $Parameters = @{
   Uri             = 'https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/color.ini'
   UseBasicParsing = $true
@@ -57,13 +57,13 @@ $Parameters = @{
 }
 Invoke-WebRequest @Parameters
 
-Write-Host -Object 'Applying...' -ForegroundColor 'Cyan'
+Write-Host -Object 'Aplicando...' -ForegroundColor 'Cyan'
 if ($isThemeInstalled -and ($currentTheme -ne 'marketplace')) {
   $Host.UI.RawUI.Flushinputbuffer()
   $choice = $Host.UI.PromptForChoice(
-    'Local theme found',
-    'Do you want to replace it with a placeholder to install themes from the Marketplace?',
-    ('&Yes', '&No'),
+    'Tema local encontrado',
+    'Você deseja substituí-lo por um espaço reservado para instalar temas do Marketplace?',
+    ('&Sim', '&Nao'),
     0
   )
   if ($choice = 1) { $setTheme = $false }
@@ -72,5 +72,8 @@ if ($setTheme) { spicetify config current_theme marketplace }
 spicetify backup
 spicetify apply
 
-Write-Host -Object 'Done!' -ForegroundColor 'Green'
-Write-Host -Object 'If nothing has happened, check the messages above for errors'
+Write-Host -Object 'Tudo Pronto!' -ForegroundColor 'Green'
+Write-Host -Object 'Se Nada Ocorreu, verifique as mensagens a cima' -ForegroundColor 'Red'
+Write-Host -Object 'Se Voce Nao Encontrou Erros A Cima, Vai Se Fuder.' -ForegroundColor 'Red'
+Write-Host -Object 'Leo Gay' -ForegroundColor 'DarkMagenta'
+
